11167f363cf508dd4620ba859ef52c3d41c05866
Franklin Wei <frankhwei536@gmail.com>
2014-03-22 20:19:39 -0400

    Implemented system

--- src/exec.c
+++ src/exec.c
@@ -1,7 +1,8 @@
 #include <alpha.h>
-word getArg(alpha_ctx* ctx)
+#include <stdio.h>
+static inline word getArg(alpha_ctx* ctx)
 {
-  if((ctx->regs).regs[3]<(ctx->memsize)-4)
+  if(ctx->regs[3]<(ctx->memsize)-4)
     {
       register word ret;
       register word pc = ctx->regs[3];
@@ -39,57 +40,154 @@ static inline word readWord(alpha_ctx* ctx, word addr)
   else
     badRead(ctx);
 }
-void exec_0(alpha_ctx* ctx, byte opcode)
+static inline word popStack(alpha_ctx* ctx)
+{
+  if(ctx->stacksize>=4)
+    {
+      register word ret=readWord(ctx, ctx->regs[2]);
+      ctx->regs[2]-=4;
+      return ret;
+    }
+  else
+    badRead(ctx);
+}
+static inline void pushStack(alpha_ctx* ctx, word value)
+{
+  if(ctx->stacksize<ctx->maxstacksize-4)
+    {
+      writeWord(ctx, ctx->regs[2], value);
+      ctx->regs[2]+=4;
+      ctx->stacksize+=4;
+    }
+  else
+    badWrite(ctx);
+}
+static void exec_0(alpha_ctx* ctx, byte opcode)
 {
   ctx->regs[opcode&0x3]=ctx->regs[(opcode&0xC)>>2];
 }
-void exec_1(alpha_ctx* ctx, byte opcode) // reg to mem
+static void exec_1(alpha_ctx* ctx, byte opcode) // reg to mem
 {
   writeWord(ctx, ctx->regs[opcode&0x3], ctx->regs[(opcode&0xC)>>2]);
 }
-void exec_2(alpha_ctx* ctx, byte opcode)
+static void exec_2(alpha_ctx* ctx, byte opcode)
 {
   ctx->regs[opcode&0x3]=readWord(ctx, ctx->regs[(opcode&0x0C)>>2]);
 }
-void exec_3(alpha_ctx* ctx, byte opcode)
+static void exec_3(alpha_ctx* ctx, byte opcode)
 {
   ctx->regs[opcode&0x3]=getArg(ctx);
   ctx->regs[3]+=4;
 }
-void exec_4(alpha_ctx* ctx, byte opcode)
+static void exec_4(alpha_ctx* ctx, byte opcode)
 {
   if(ctx->regs[0]==ctx->regs[(opcode&0x0C)>>2])
     {
       ctx->regs[3]=ctx->regs[opcode&0x03]-1; // no bounds checking here, done elsewhere
     }
 }
-void exec_5(alpha_ctx* ctx, byte opcode)
+static void exec_5(alpha_ctx* ctx, byte opcode)
 {
   if(ctx->regs[0]!=ctx->regs[(opcode&0x0C)>>2])
     {
       ctx->regs[3]=ctx->regs[opcode&0x03]-1; // no bounds checking here, done elsewhere
     }
 }
-void exec_6(alpha_ctx* ctx, byte opcode)
+static void exec_6(alpha_ctx* ctx, byte opcode)
 {
   if(ctx->regs[0]<ctx->regs[(opcode&0x0C)>>2])
     {
       ctx->regs[3]=ctx->regs[opcode&0x03]-1; // no bounds checking here, done elsewhere
     }
 }
-void exec_7(alpha_ctx* ctx, byte opcode)
+static void exec_7(alpha_ctx* ctx, byte opcode)
 {
   if(ctx->regs[0]>ctx->regs[(opcode&0x0C)>>2])
     {
       ctx->regs[3]=ctx->regs[opcode&0x03]-1; // no bounds checking here, done elsewhere
     }
 }
+static void exec_8(alpha_ctx* ctx, byte opcode)
+{
+  byte detail=(opcode&0x0C)>>2;
+  switch(detail)
+    {
+    case 0:
+      pushStack(ctx, ctx->regs[opcode&0x3]);
+      break;
+    case 1:
+      ctx->regs[opcode&0x03]=popStack(ctx);
+      break;
+    case 2:
+      ctx->regs[0]+=ctx->regs[opcode&0x03];
+      break;
+    case 3:
+      ctx->regs[0]-=ctx->regs[opcode&0x03];
+      break;
+      // no need for default with 2 bits!
+    }
+}
+static void exec_9(alpha_ctx* ctx, byte opcode)
+{
+  switch((opcode&0x0C)>>2)
+  {
+  case 0:
+    ctx->regs[0]*=ctx->regs[opcode&0x03];
+    break;
+  case 1:
+    ctx->regs[0]/=ctx->regs[opcode&0x03];
+    break;
+  case 2:
+    ctx->regs[0]%=ctx->regs[opcode&0x03];
+      break;
+  case 3:
+    --(ctx->regs[opcode&0x03]);
+    break;
+  }
+}
+static void exec_A(alpha_ctx* ctx, byte opcode)
+{
+  ++(ctx->regs[opcode&0x03]);
+}
+static void exec_C(alpha_ctx* ctx, byte opcode)
+{
+  switch((opcode&0x3C)>>2)
+    {
+    case 0x0:
+      printf("%u", ctx->regs[0]);
+      break;
+    case 0x01:
+      ctx->regs[0]=ctx->memsize;
+      break;
+    case 0x02: // call
+      pushStack(ctx, (ctx->regs[3]));
+      ctx->regs[3]=ctx->regs[opcode&0x03]-1;
+      break;
+    case 0x03: // ret
+      ctx->regs[3]=popStack(ctx);
+      break;
+    case 0x04:
+      ctx->return_value=ctx->regs[0];
+      ctx->done=true;
+      break;
+    case 0x05:
+      --(ctx->regs[0]);
+      if(ctx->regs[0])
+	ctx->regs[3]=ctx->regs[opcode&0x03]-1;
+      break;
+    default:
+      badInstr(ctx);
+    }
+}
+static void nop(alpha_ctx* ctx, byte opcode)
+{
+  return;
+}
 void exec_opcode(alpha_ctx* ctx, byte opcode)
 {
-  void (*first_stage_exec[16])(alpha_ctx*, byte)={ // table of handlers for first nibble in opcode 
+  static void (*first_stage_exec[16])(alpha_ctx*, byte)={ // table of handlers for first nibble in opcode 
   &exec_0, &exec_1, &exec_2, &exec_3, &exec_4,
   &exec_5, &exec_6, &exec_7, &exec_8, &exec_9,
-  &exec_A, &exec_B, &exec_C, &exec_D, &exec_E,
-  &exec_F};
+  &exec_A, &nop, &exec_C, &nop, &nop, &nop};
   first_stage_exec[(opcode&0xF0)>>4](ctx, opcode);
 }
