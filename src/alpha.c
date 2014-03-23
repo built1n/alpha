0e967c5274aaa618bf9bf8fc2cc4eaa9f13da0ee
Franklin Wei <frankhwei536@gmail.com>
2014-03-22 20:19:21 -0400

    Implemented bad... functions

--- src/alpha.c
+++ src/alpha.c
@@ -1,12 +1,23 @@
 #include <alpha.h>
+#include <stdlib.h>
+#include <stdio.h>
 void badRead(alpha_ctx* ctx)
 {
+  printf("Bad read.\n");
+  ctx->done=true;
+  ctx->return_value=ALPHA_OUT_OF_BOUNDS;
 }
 void badWrite(alpha_ctx* ctx)
 {
+  printf("Bad write.\n");
+  ctx->done=true;
+  ctx->return_value=ALPHA_OUT_OF_BOUNDS;
 }
 void badInstr(alpha_ctx* ctx)
 {
+  printf("Invalid instruction.\n");
+  ctx->done=true;
+  ctx->return_value=ALPHA_BAD_INSTR;
 }
 inline static byte readByte(alpha_ctx* ctx, word addr)
 {
@@ -15,8 +26,21 @@ inline static byte readByte(alpha_ctx* ctx, word addr)
   else
     badRead(ctx);
 }
+alpha_ctx* alpha_init(byte* mem, word sz, word stackSz, word sp)
+{
+  alpha_ctx* ctx=malloc(sizeof(alpha_ctx));
+  ctx->memory=mem;
+  ctx->memsize=sz;
+  ctx->maxstacksize=stackSz;
+  ctx->regs[2]=sp;
+  ctx->regs[0]=rand();
+  ctx->return_value=EXIT_SUCCESS;
+}
 void alpha_exec(alpha_ctx* ctx)
 {
-  exec_opcode(ctx, readByte(ctx, ctx->regs[3]));
-  ++(ctx->regs[3]);
+  if(!ctx->done)
+    {
+      exec_opcode(ctx, readByte(ctx, ctx->regs[3]));
+      ++(ctx->regs[3]);
+    }
 }
