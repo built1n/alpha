f047416b7964ed1383a36309f00d728d7617ff12
Franklin Wei <frankhwei536@gmail.com>
2014-03-25 19:16:04 -0400

    Removed some unused static functions

--- src/disasm.c
+++ src/disasm.c
@@ -17,55 +17,6 @@ static inline word getArg(alpha_ctx* ctx)
     badRead(ctx);
   return 0xDEADBEEF;
 }
-static inline void writeWord(alpha_ctx* ctx, word addr, word value)
-{
-  if(addr<ctx->memsize-3)
-    {
-      ctx->memory[addr]=((value&0xFF000000) >> 24);
-      ctx->memory[addr+1]=((value&0x00FF0000) >> 16);
-      ctx->memory[addr+2]=((value&0x0000FF00) >> 8);
-      ctx->memory[addr+3]=((value&0xFF));
-    }
-  else
-    badWrite(ctx);
-}
-static inline word readWord(alpha_ctx* ctx, word addr)
-{
-  if(addr<ctx->memsize-3)
-    {
-      word ret=ctx->memory[addr+3];
-      ret|=((ctx->memory[addr+2])<<8);
-      ret|=((ctx->memory[addr+1])<<16);
-      ret|=((ctx->memory[addr])<<24);
-      return ret;
-    }
-  else
-    badRead(ctx);
-  return 0xDEADBEEF;
-}
-static inline word popStack(alpha_ctx* ctx)
-{
-  if(ctx->stacksize>=4)
-    {
-      register word ret=readWord(ctx, ctx->regs[2]);
-      ctx->regs[2]-=4;
-      return ret;
-    }
-  else
-    badRead(ctx);
-  return 0xDEADBEEF;
-}
-static inline void pushStack(alpha_ctx* ctx, word value)
-{
-  if(ctx->stacksize<ctx->maxstacksize-4)
-    {
-      writeWord(ctx, ctx->regs[2], value);
-      ctx->regs[2]+=4;
-      ctx->stacksize+=4;
-    }
-  else
-    badWrite(ctx);
-}
 static void exec_0(alpha_ctx* ctx, byte opcode)
 {
   if((opcode&3)!=(opcode&0xC)>>2)
