e3d643e4766bc9b2ac8ed00cc29f7914c248a113
Franklin Wei <frankhwei536@gmail.com>
2014-04-01 13:43:37 -0400

    Implemented disassembler

--- src/disasm.c
+++ src/disasm.c
@@ -1,180 +1,260 @@
 #include <alpha.h>
 #include <util.h>
 #include <stdio.h>
-#include <stdlib.h>
 static inline word getArg(alpha_ctx* ctx)
 {
-  if(ctx->regs[3]<(ctx->memsize)-4)
+  if(ctx->regs[PC]+1<(ctx->memsize)-4)
     {
       register word ret;
-      register word pc = ctx->regs[3];
-      ret=(ctx->memory[pc+1]<<24);
-      ret|=(ctx->memory[pc+2]<<16);
-      ret|=(ctx->memory[pc+3]<<8);
-      ret|=(ctx->memory[pc+4]);
+      register word pc = ctx->regs[PC]+1;
+      ret=(ctx->memory[pc]<<24);
+      ret|=(ctx->memory[pc+1]<<16);
+      ret|=(ctx->memory[pc+2]<<8);
+      ret|=(ctx->memory[pc+3]);
+      ctx->regs[PC]+=4;
       return ret;
     }
   else
-    {
-      badRead(ctx);
-      exit(1);
-    }
+    badRead(ctx);
   return 0xDEADBEEF;
 }
-static void exec_0(alpha_ctx* ctx, byte opcode)
+static inline void writeWord(alpha_ctx* ctx, word addr, word value)
 {
-  if((opcode&3)!=(opcode&0xC)>>2)
-    printf("MOV R%1d, R%1d\n", opcode&0x03, (opcode&0xC)>>2);
-  else
-    printf("NOP\n");
-}
-static void exec_1(alpha_ctx* ctx, byte opcode) // reg to mem
-{
-  printf("MOV (R%1d), R%1d\n", opcode&0x03, (opcode&0xC)>>2);
-}
-static void exec_2(alpha_ctx* ctx, byte opcode)
-{
-  printf("MOV R%1d, (R%1d)\n", opcode&0x03, (opcode&0xC)>>2);
-}
-static void exec_3(alpha_ctx* ctx, byte opcode)
-{
-  printf("MOV R%1d, $0x%08X\n", opcode&3, getArg(ctx));
-  ctx->regs[3]+=4;
-}
-static void exec_4(alpha_ctx* ctx, byte opcode)
-{
-  printf("JE R%1d, R%1d\n", (opcode&0xC)>>2, opcode&0x3);
-}
-static void exec_5(alpha_ctx* ctx, byte opcode)
-{
-  printf("JNE R%1d, R%1d\n", (opcode&0xC)>>2, opcode&0x3);
-}
-static void exec_6(alpha_ctx* ctx, byte opcode)
-{
-  printf("JL R%1d, R%1d\n",(opcode&0xC)>>2, opcode&0x3);
-}
-static void exec_7(alpha_ctx* ctx, byte opcode)
-{
-  printf("JG R%1d, R%1d\n", (opcode&0xC)>>2, opcode&0x3);
-}
-static void exec_8(alpha_ctx* ctx, byte opcode)
-{
-  byte detail=(opcode&0x0C)>>2;
-  switch(detail)
+  if(addr<ctx->memsize-3)
     {
-    case 0:
-      printf("PUSH R%1d\n", opcode&0x3);
+      ctx->memory[addr]=((value&0xFF000000) >> 24);
+      ctx->memory[addr+1]=((value&0x00FF0000) >> 16);
+      ctx->memory[addr+2]=((value&0x0000FF00) >> 8);
+      ctx->memory[addr+3]=((value&0xFF));
+    }
+  else
+    badWrite(ctx);
+}
+static inline word readWord(alpha_ctx* ctx, word addr)
+{
+  if(addr<ctx->memsize-3)
+    {
+      word ret=ctx->memory[addr+3];
+      ret|=((ctx->memory[addr+2])<<8);
+      ret|=((ctx->memory[addr+1])<<16);
+      ret|=((ctx->memory[addr])<<24);
+      return ret;
+    }
+  else
+    badRead(ctx);
+  return 0xDEADBEEF;
+}
+static inline word popStack(alpha_ctx* ctx)
+{
+  if(ctx->stacksize>=4)
+    {
+      register word ret=readWord(ctx, ctx->regs[SP]);
+      ctx->regs[SP]-=4;
+      return ret;
+    }
+  else
+    badRead(ctx);
+  return 0xDEADBEEF;
+}
+static inline void pushStack(alpha_ctx* ctx, word value)
+{
+  if(ctx->stacksize<ctx->maxstacksize-4)
+    {
+      writeWord(ctx, ctx->regs[SP], value);
+      ctx->regs[SP]+=4;
+      ctx->stacksize+=4;
+    }
+  else
+    badWrite(ctx);
+}
+static void reg_to_reg(alpha_ctx* ctx, byte operand)
+{
+  printf("MOV R%d, R%d\n", operand&0xF, (operand&0xF0)>>4);
+}
+static void mem_to_reg(alpha_ctx* ctx, byte operand)
+{
+  printf("MOV R%d, (R%d)\n", operand&0xF, (operand&0xF0)>>4);
+}
+static void reg_to_mem(alpha_ctx* ctx, byte operand)
+{
+  printf("MOV (R%d), R%d\n", operand&0xF, (operand&0xF0)>>4);
+}
+static void load_imm(alpha_ctx* ctx, byte operand)
+{
+  printf("MOV R%d, $0x%08X\n", operand&0xF, getArg(ctx));
+}
+static void jump_equ(alpha_ctx* ctx, byte operand)
+{
+  printf("JE R%d, R%d\n", (operand&0xF0)>>4, operand&0xF);
+}
+static void jump_less(alpha_ctx* ctx, byte operand)
+{
+  printf("JL R%d, R%d\n", (operand&0xF0)>>4, operand&0xF);
+}
+static void jump_greater(alpha_ctx* ctx, byte operand)
+{
+  printf("JG R%d, R%d\n", (operand&0xF0)>>4, operand&0xF);
+}
+static void jump_noequ(alpha_ctx* ctx, byte operand)
+{
+  printf("JNE R%d, R%d\n", (operand&0xF0)>>4, operand&0xF);
+}
+static void djnz(alpha_ctx* ctx, byte operand)
+{
+  printf("DJNZ R%d, R%d\n", (operand&0xF0)>>4, operand&0xF);
+}
+static void add_reg(alpha_ctx* ctx, byte operand)
+{
+  printf("ADD R%d, R%d\n", operand&0xF, (operand&0xF0)>>4);
+}
+static void sub_reg(alpha_ctx* ctx, byte operand)
+{
+  printf("SUB R%d, R%d\n", operand&0xF, (operand&0xF0)>>4);
+}
+static void mul_reg(alpha_ctx* ctx, byte operand)
+{
+  printf("MUL R%d, R%d\n", operand&0xF, (operand&0xF0)>>4);
+}
+static void div_reg(alpha_ctx* ctx, byte operand)
+{
+  printf("DIV R%d, R%d\n", operand&0xF, (operand&0xF0)>>4);
+}
+static void add_imm(alpha_ctx* ctx, byte operand)
+{
+  printf("ADD R%d, $0x%08X\n", operand&0xF, getArg(ctx));
+}
+static void sub_imm(alpha_ctx* ctx, byte operand)
+{
+  printf("SUB R%d, $0x%08X\n", operand&0xF, getArg(ctx));
+}
+static void mul_imm(alpha_ctx* ctx, byte operand)
+{
+  printf("MUL R%d, $0x%08X\n", operand&0xF, getArg(ctx));
+}
+static void div_imm(alpha_ctx* ctx, byte operand)
+{
+  printf("DIV R%d, $0x%08X\n", operand&0xF, getArg(ctx));
+}
+static void incr(alpha_ctx* ctx, byte operand)
+{
+  printf("INCR R%d\n", operand&0xF);
+}
+static void decr(alpha_ctx* ctx, byte operand)
+{
+  printf("DECR R%d\n", operand&0xF);
+}
+static void push_reg(alpha_ctx* ctx, byte operand)
+{
+  printf("PUSH R%d\n", operand&0xF);
+}
+static void push_imm(alpha_ctx* ctx, byte operand)
+{
+  ctx->regs[PC]-=1; // BUG?
+  printf("PUSH $ 0x%08X\n", getArg(ctx));
+}
+static void pop(alpha_ctx* ctx, byte operand)
+{
+  printf("POP R%d\n", operand&0xF);
+}
+static void call_reg(alpha_ctx* ctx, byte operand)
+{
+  printf("CALL (R%d)\n", operand&0xF);
+}
+static void call_imm(alpha_ctx* ctx, byte operand)
+{
+  --ctx->regs[PC];
+  printf("CALL $0x%08X\n", getArg(ctx));
+}
+static void ret(alpha_ctx* ctx, byte operand)
+{
+  printf("RET\n");
+}
+static void alpha_putchar_imm(alpha_ctx* ctx, byte c)
+{
+  printf("PUTC %c\n",c);
+}
+static void alpha_putchar_reg(alpha_ctx* ctx, byte operand)
+{
+  printf("PUTC R%d\n", operand&0xF);
+}
+static void puts_reg(alpha_ctx* ctx, byte operand)
+{
+  printf("PUTS R%d\n", operand&0xF);
+}
+static void print_number(alpha_ctx* ctx, byte operand)
+{
+  byte reg_num=(operand&0xF);
+  switch((operand&0xF0)>>4)
+    {
+    case 0: // decimal
+      printf("PUTDECU ");
       break;
     case 1:
-      printf("POP R%1d\n", opcode&0x3);
+      printf("PUTHEXU ");
       break;
     case 2:
-      printf("ADD R%1d\n", opcode&0x3);
+      printf("PUTHEXUP ");
       break;
     case 3:
-      printf("SUB R%1d\n", opcode&0x3);
+      printf("PUTHEXL ");
       break;
-      // no need for default with 2 bits!
-    }
-}
-static void exec_9(alpha_ctx* ctx, byte opcode)
-{
-  switch((opcode&0x0C)>>2)
-  {
-  case 0:
-    printf("MUL R%1d\n", opcode&0x3);
-    break;
-  case 1:
-    printf("DIV R%1d\n", opcode&0x3);
-    break;
-  case 2:
-    printf("MOD R%1d\n", opcode&0x3);
-    break;
-  case 3:
-    printf("DEC R%1d\n", opcode&0x3);
-    break;
-  }
-}
-static void exec_A(alpha_ctx* ctx, byte opcode)
-{
-  printf("INC R%1d\n", opcode&0x3);
-}
-static void exec_extd(alpha_ctx* ctx, byte opcode)
-{
-  switch((opcode&0x3C)>>2)
-    {
-    case 0x0:
-      printf("PRINT\n");
+    case 4:
+      printf("PUTHEXLP ");
       break;
-    case 0x01:
-      printf("GETMEMSZ\n");
-    case 0x02: // call
-      printf("CALL R%1d\n", opcode&0x03);
-      break;
-    case 0x03: // ret
-      printf("RET\n");
-      break;
-    case 0x04:
-      printf("HLT\n");
-      break;
-    case 0x05:
-      printf("DJNZ R%1d\n", opcode&0x03);
-      break;
-    case 0x06:
-      {
-	printf("PUTS R%1d\n", opcode&0x03);
-	break;
-      }
-    case 0x07:
-      printf("JMP R%1d\n", opcode&3);
-      break;
-    case 0x08:
-      switch(opcode&3)
-	{
-	case 0:
-	  printf("NL\n");
-	  break;
-	case 1:
-	  printf("JR 0x%02X\n", readByte(ctx, ctx->regs[3]+1));
-	  ++ctx->regs[3];
-	  break;
-	case 2:
-	  printf("NOT\n");
-	  break;
-	case 3:
-	  printf("LROT\n");
-	  break;
-	  // no default here
-	}
-      break;
-    case 0x09:
-      printf("AND R%1d\n", opcode&3);
-      break;
-    case 0xA:
-      printf("OR R%1d\n", opcode&3);
-      break;
-    case 0x0B:
-      printf("XOR R%1d\n", opcode&3);
-      break;
-    case 0x0C:
-      printf("LSH R%1d\n", opcode&3);
-      break;
-    case 0x0D:
-      printf("RSH R%1d\n", opcode&3);
+    case 5:
+      printf("PUTDECS ");
       break;
     default:
-      badInstr(ctx);
+      printf("Unknown instruction\n");
+      return;
     }
+  printf("R%d\n", reg_num);
 }
-static void unknown_opcode(alpha_ctx* ctx, byte opcode)
+static void halt_execution(alpha_ctx* ctx, byte operand)
 {
-  printf("DATA 0x%02X\n", opcode);
+  printf("HLT R%d\n", ctx->regs[operand&0xF]);
 }
-void disasm_opcode(alpha_ctx* ctx, byte opcode)
+void disasm_opcode(alpha_ctx* ctx, byte opcode, byte operand)
 {
-  static void (*exec_table[16])(alpha_ctx*, byte)={ // table of handlers for first nibble in opcode 
-  &exec_0, &exec_1, &exec_2, &exec_3, &exec_4,
-  &exec_5, &exec_6, &exec_7, &exec_8, &exec_9,
-  &exec_A, &unknown_opcode, &exec_extd, &exec_extd, &exec_extd/* 0xC, 0xD, and 0x0E are extd. instructions*/, &unknown_opcode};
-  exec_table[(opcode&0xF0)>>4](ctx, opcode);
+  static void (*exec_table[256])(alpha_ctx*, byte)={
+    &reg_to_reg, // 0x00
+    &mem_to_reg, // 0x01
+    &reg_to_mem, // 0x02
+    &load_imm, // 0x03
+
+    &jump_equ, // 0x04
+    &jump_less, // 0x05
+    &jump_greater, // 0x06
+    &jump_noequ, // 0x07
+    &djnz, // 0x08
+
+    &add_reg, // 0x09
+    &sub_reg, // 0x0A
+    &mul_reg, // 0x0B
+    &div_reg, // 0x0C
+
+    &add_imm, // 0x0D
+    &sub_imm, // 0x0E
+    &mul_imm, // 0x0F
+    &div_imm, // 0x10
+    &incr, // 0x11
+    &decr, // 0x12
+
+    &push_reg, // 0x13
+    &push_imm, // 0x14
+    &pop, // 0x15
+    &call_reg, // 0x16
+    &call_imm, // 0x17
+    &ret, // 0x18
+
+    &alpha_putchar_imm, // 0x19
+    &alpha_putchar_reg, // 0x1A
+    &puts_reg, // 0x1B
+    &print_number, // 0x1C
+    
+    &halt_execution // 0x1D
+    // more!
+  };
+  if(exec_table[opcode])
+    exec_table[opcode](ctx, operand);
 }
