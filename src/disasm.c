f900f691db962178f21509eb859586c8851139d5
Franklin Wei <frankhwei536@gmail.com>
2014-04-02 10:11:14 -0400

    Took utility functions out of exec.c and disasm.c moved into memory.h

--- src/disasm.c
+++ src/disasm.c
@@ -1,72 +1,7 @@
 #include <alpha.h>
 #include <util.h>
 #include <stdio.h>
-static inline word getArg(alpha_ctx* ctx)
-{
-  if(ctx->regs[PC]+2<(ctx->memsize)-4)
-    {
-      register word ret;
-      register word pc = ctx->regs[PC]+2;
-      ret=(ctx->memory[pc]<<24);
-      ret|=(ctx->memory[pc+1]<<16);
-      ret|=(ctx->memory[pc+2]<<8);
-      ret|=(ctx->memory[pc+3]);
-      ctx->regs[PC]+=4;
-      return ret;
-    }
-  else
-    badRead(ctx);
-  return 0xDEADBEEF;
-}
-static inline void writeWord(alpha_ctx* ctx, word addr, word value)
-{
-  if(addr<ctx->memsize-3)
-    {
-      ctx->memory[addr]=((value&0xFF000000) >> 24);
-      ctx->memory[addr+1]=((value&0x00FF0000) >> 16);
-      ctx->memory[addr+2]=((value&0x0000FF00) >> 8);
-      ctx->memory[addr+3]=((value&0xFF));
-    }
-  else
-    badWrite(ctx);
-}
-static inline word readWord(alpha_ctx* ctx, word addr)
-{
-  if(addr<ctx->memsize-3)
-    {
-      word ret=ctx->memory[addr+3];
-      ret|=((ctx->memory[addr+2])<<8);
-      ret|=((ctx->memory[addr+1])<<16);
-      ret|=((ctx->memory[addr])<<24);
-      return ret;
-    }
-  else
-    badRead(ctx);
-  return 0xDEADBEEF;
-}
-static inline word popStack(alpha_ctx* ctx)
-{
-  if(ctx->stacksize>=4)
-    {
-      register word ret=readWord(ctx, ctx->regs[SP]);
-      ctx->regs[SP]-=4;
-      return ret;
-    }
-  else
-    badRead(ctx);
-  return 0xDEADBEEF;
-}
-static inline void pushStack(alpha_ctx* ctx, word value)
-{
-  if(ctx->stacksize<ctx->maxstacksize-4)
-    {
-      writeWord(ctx, ctx->regs[SP], value);
-      ctx->regs[SP]+=4;
-      ctx->stacksize+=4;
-    }
-  else
-    badWrite(ctx);
-}
+#include <memory.h>
 static void reg_to_reg(alpha_ctx* ctx, byte operand)
 {
   printf("MOV R%d, R%d\n", operand&0xF, (operand&0xF0)>>4);
